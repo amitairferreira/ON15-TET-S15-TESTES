e52312ebb69445cabed7c6acddb558d755e367ab
amitairferreira <amitairlima@gmail.com>
2022-07-06 23:41:21 -0300

    Aula

--- Aula/src/routes/userRoutes.js
+++ Aula/src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const controller = require("../controllers/userController");
 const authController = require("../controllers/authController");
 
+
 router.get("/all", controller.getAll);
 
 router.post("/create", controller.createUser);
